d8ed07aa86578641f406dc5cbd27c72d2aa70c7a
vsyntd <120342640+vsyntd@users.noreply.github.com>
2025-05-21 14:35:26 +0200

    nochmal

--- scr/HelloWorld.java
+++ scr/HelloWorld.java
@@ -11,6 +11,6 @@ public class HelloWorld
     public static void main(String args[]){
         System.out.println("Hello World");
         System.out.println("Hello World!");
-        System.out.println("Hello iuhzgiuzgiz!");
+        System.out.println("Hello new world!");
     }
 }
