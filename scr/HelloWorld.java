ffb72283b1baaef57f7dc9eeacac947ea86753d0
David <david@david.com>
2025-05-21 14:18:14 +0200

    erste änderung

---
+++ scr/HelloWorld.java
@@ -0,0 +1,14 @@
+
+/**
+ * Klasse HelloWorld.
+ * 
+ * @author (Ihr Name) 
+ * @version (eine Versionsnummer oder ein Datum)
+ */
+
+public class HelloWorld
+{
+    public static void main(String args[]){
+        System.out.println("Hello World");
+    }
+}
